bb39c00af4cfe2972f3b4161c4045c50f313b706
Vishnuu011 <vishnurrajeev@gmail.com>
2025-09-05 07:48:13 -0700

    add.tools.queryrewiter

---
+++ src/astrasearch/query_rewriter/query_rewriter.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+
+from langchain_core.runnables import RunnableSequence
+
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from typing import Optional, List, Literal
+
+load_dotenv()
+
+
+class StructuredQuery(BaseModel):
+
+    query: str = Field(
+        ...,
+        description="Cleaned user query. Grammar fixed. If stock-related, use yfinance symbol."
+    )
+
+
+
+def query_rewriter_agent(model_name: str, temperature: float | int ) -> Optional[RunnableSequence]:
+
+    try:
+        system_prompt = system_prompt = """You are a query refiner and normalizer.
+
+        Your tasks:
+        1. Take the user's raw query and rewrite it into a clean, well-structured query.
+           - Fix grammar mistakes.
+           - Remove unnecessary words.
+           - Make it concise and clear.                
+
+        2. If the query is related to the stock market:
+           - Identify the company name or ticker symbol.
+           - If the company is Indian (NSE), append `.NS` to the symbol.
+             Example: "TCS stock market analysis" → "TCS.NS"
+                      "Reliance stock price today" → "RELIANCE.NS"
+           - Otherwise, use the correct Yahoo Finance symbol (e.g., Apple → AAPL).        
+
+        3. Return only the cleaned query as structured JSON.
+        """
+
+        llm = ChatGroq(
+            model=model_name,
+            temperature=temperature
+        )
+
+        structuredllm = llm.with_structured_output(
+            StructuredQuery
+        )
+
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            ("human", "{user_input}")
+        ])
+
+        chain = (
+            prompt
+            | structuredllm
+        )
+
+        return chain
+    except Exception as e:
+        print(f"ERROR IN QUERY RE-WRITER: {e}")
---
+++ src/astrasearch/tools/tools.py
@@ -0,0 +1,134 @@
+import requests
+import os
+import arxiv
+from dotenv import load_dotenv
+from googleapiclient.discovery import build
+from langchain.tools import tool
+import yfinance as yf
+
+load_dotenv()
+
+SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+
+
+def google_search(queary: str) -> list[dict]:
+
+    """Searches Google for the given query."""
+
+    try:
+        url = "https://google.serper.dev/search"
+        payload = {
+            "q":queary
+        }
+        headers = {
+            "X-API-KEY": SERPER_API_KEY, 
+            "Content-Type": "application/json"
+        }
+        res = requests.post(
+            url=url,
+            json=payload,
+            headers=headers
+        )
+        results = res.json().get("organic", [])
+
+        return [
+            {"title": r["title"], "link": r["link"], "meta": r.get("snippet", "")}
+            for r in results[:10]
+        ]
+
+    except Exception as e:
+        print(f"[ERROR in google_search] : {e}")
+
+       
+
+def arxiv_search(query: str) -> list[dict]:
+
+    """Searches ArXiv for the given query."""
+
+    try:
+        search = arxiv.Search(
+            query=query, 
+            max_results=5, 
+            sort_by=arxiv.SortCriterion.Relevance
+        )
+        results = []
+        for result in search.results():
+            results.append({
+                "title": result.title,
+                "link": result.entry_id,
+                "meta": ", ".join(a.name for a in result.authors)
+            })
+        return results   
+        
+    except Exception as e:
+        print(f"[ERROR in arxiv_search] : {e}")
+
+
+ 
+
+def youtube_search(query: str) -> list[dict]:
+
+    """Searches youtube for the given query."""
+
+    try:
+        youtube = build(
+            "youtube", 
+            "v3", 
+            developerKey=YOUTUBE_API_KEY
+        )
+        req = youtube.search().list(
+            q=query, 
+            part="snippet", 
+            type="video", 
+            maxResults=5
+        )
+        res = req.execute()
+        return [
+            {
+                "title": item["snippet"]["title"],
+                "url": f"https://www.youtube.com/watch?v={item['id']['videoId']}",
+                "channel": item["snippet"]["channelTitle"],
+            }
+            for item in res.get("items", [])
+        ]       
+    except Exception as e:
+        print(f"[ERROR in youtubesearch] : {e}")
+
+
+
+@tool("Live Stock Information Tool")
+def get_stock_price(stock_symbol: str) -> str:
+
+    """
+    Retrieves the latest stock price and other relevant info for a given stock symbol using Yahoo Finance.
+
+    Parameters:
+        stock_symbol (str): The ticker symbol of the stock (e.g., AAPL, TSLA, MSFT).
+
+    Returns:
+        str: A summary of the stock's current price, daily change, and other key data.
+    """
+  
+    try:
+        stock = yf.Ticker(stock_symbol)
+        info = stock.info
+
+        current_price = info.get("regularMarketPrice")
+        change = info.get("regularMarketChange")
+        change_percent = info.get("regularMarketChangePercent")
+        currency = info.get("currency", "USD")
+
+        if current_price is None:
+            return f"Could not fetch price for {stock_symbol}. Please check the symbol."
+
+        return (
+            f"Stock: {stock_symbol.upper()}\n"
+            f"Price: {current_price} {currency}\n"
+            f"Change: {change} ({round(change_percent, 2)}%)"
+        )
+    
+    except Exception as e:
+        print(f"[ERROR get_stock_price (tool)]: {e}")        
